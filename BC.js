2b2cfed21b132a6d293dc3c7f9a949cd1dbd05a6
Shamika02 <71770711+Shamika02@users.noreply.github.com>
2020-12-13 19:08:02 -0500

    Add files via upload

---
+++ BC.js
@@ -0,0 +1,24 @@
+
+function add(){
+    fst=parseInt(myShamika.fst.value);
+    snd=parseInt(myShamika.snd.value);
+    result=fst+snd;
+    myShamika.result.value=result;
+}
+
+function subtract(){
+    fst=parseInt(myShamika.fst.value);
+    snd=parseInt(myShamika.snd.value);
+    result=fst-snd;
+    myShamika.result.value=result;
+   
+}
+
+function multiply(){
+    fst=parseInt(myShamika.fst.value);
+    snd=parseInt(myShamika.snd.value);
+    result=fst*snd;
+    myShamika.result.value=result;
+}
+document.body.style.color="Purple"
+document.body.style.backgroundColor="yellow"
