704f08afa3bb55377acf4d9303b34fce41d37da7
Shamika02 <71770711+Shamika02@users.noreply.github.com>
2020-12-16 09:54:35 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ console.log(inputBox.value)
 var inputBox = document.getElementById("radio-input")
 console.log(inputBox.value)
 
+function myFunction(){
+    alert("Your results have been submitted");
+}
 var inputBox = document.getElementById("submit")
 console.log(inputBox.value)
