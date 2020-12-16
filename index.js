95f82cf88c3739ddfeeba2962748926845f24c3e
Shamika02 <71770711+Shamika02@users.noreply.github.com>
2020-12-16 09:36:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var inputBox = document.getElementById("shamika-input")
+console.log(inputBox.value)
+
+var inputBox = document.getElementById("radio-input")
+console.log(inputBox.value)
+
+var inputBox = document.getElementById("submit")
+console.log(inputBox.value)
