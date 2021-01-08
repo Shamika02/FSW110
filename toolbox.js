de6c542a9e712b0c95949d298b69648cf44805d5
Shamika02 <71770711+Shamika02@users.noreply.github.com>
2021-01-08 10:10:15 -0500

    Add files via upload

--- toolbox.js
+++ toolbox.js
@@ -1,8 +1,26 @@
-function mytodoList(){
-    var item= document.getElementById("mytodoInput").value
-    var text= document.createTextNode(item)
-    var newItem= document.createElement("li")
-    newItem.appendChild(text)
-    document.getElementById("mytodoList").appendChild(newItem)
+var row = 0;
+function addText(){
+    var input = document.getElementById('inputTask').value;
+    if(input != "")
+    {
+    var node=document.createElement("p");
+    var textnode=document.createTextNode(input);
+    node.appendChild(textnode);
+        node.setAttribute("id","shamika"+row);
+    document.getElementById('mytodoList').appendChild(node);
+
+    var removeTask = document.createElement('input');
+    removeTask.setAttribute('type', 'button');
+    removeTask.setAttribute("value", "Remove");
+    removeTask.setAttribute("id", "removeButton");
+    removeTask.setAttribute("onClick", "deleterow("+ row +");");
+
+    node.appendChild(removeTask);
+        row++;
+    }
+}
+function deleterow(ID)
+{
+    document.getElementById('shamika'+ID).remove();
 }
 
