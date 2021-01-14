7de19fb4b7ee849514297276ec8e8a7f33704485
Shamika02 <71770711+Shamika02@users.noreply.github.com>
2021-01-13 20:06:32 -0500

    Add files via upload

---
+++ capstoneE.js
@@ -0,0 +1,25 @@
+function add(){
+    fst=parseInt(myShamika.fst.value);
+    snd=parseInt(myShamika.snd.value);
+    trd=parseInt(myShamika.trd.value);
+    result=fst+snd+trd;
+    myShamika.result.value=result;
+   
+}
+
+function subtract(){
+    fst=parseInt(myShamika.fst.value);
+    snd=parseInt(myShamika.snd.value);
+    trd=parseInt(myShamika.trd.value);
+    result=fst-snd-trd;
+    myShamika.result.value=result;
+   
+}
+
+function multiply(){
+    fst=parseInt(myShamika.fst.value);
+    snd=parseInt(myShamika.snd.value);
+    trd=parseInt(myShamika.trd.value);
+    result=fst*snd*trd;
+    myShamika.result.value=result;
+}
